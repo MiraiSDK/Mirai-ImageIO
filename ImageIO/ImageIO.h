d665414bc3afe293086e009f6e39dc70fa1b98ac
Yonghui Chen <chyhfj@gmail.com>
2015-04-10 14:31:29 +0800

    Initial Commit

---
+++ ImageIO/ImageIO.h
@@ -0,0 +1,19 @@
+//
+//  ImageIO.h
+//  ImageIO
+//
+//  Created by Chen Yonghui on 4/10/15.
+//  Copyright (c) 2015 Shanghai TinyNetwork Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ImageIO.
+FOUNDATION_EXPORT double ImageIOVersionNumber;
+
+//! Project version string for ImageIO.
+FOUNDATION_EXPORT const unsigned char ImageIOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageIO/PublicHeader.h>
+
+
